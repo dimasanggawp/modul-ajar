81e892b19d561c2b38ed31ccca1edfbcdb168ec6
Dimas Angga Wahyu Putra <dimasanggawp@gmail.com>
2026-02-06 17:24:16 +0700

    modified:   src/App.jsx 	modified:   vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import DeepLearningGenerator from './pages/DeepLearningGenerator';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/generator" element={<DeepLearningGenerator />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
